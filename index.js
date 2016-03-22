a965b469b229e0435e5e06ad2c26410f533c2717
Alejandro Irimia <rimax_86@hotmail.com>
2016-03-22 20:55:13 +0100

    Objetos de jQuery

--- index.js
+++ index.js
@@ -11,14 +11,47 @@ window.onload = function () {
 //$(document).ready(function(){})
 //$().ready(function(){})
 $(function(){
-	// Elemento del DOM
-	var header = document.getElementById('#app-header');
-	// $header es un jQuery Object (Mas funcionalidades)
-	var $header = $(header);
+	// Mejor hacer seleccion con ID ya que internamente jQuery
+	// llama a getElemByID y luego buscar el elemento que
+	// necesitamos. Es mas rapido que
+	//$('#app-header h1')
+	$('#app-header').find('h1');
 
-	var title = $('h1', header);
-	var title = $('h1', $header[0]);
-	console.log(title);
+	// Creacion de elementos con jQuery
+	var $a = $('<a>', {
+		href: 'http://platzi.com',
+		target: '_blank',
+		html: 'Ir a Platzi'
+	})
+	$('#app-body').append($a);
+
+	// Metodo Getter (opera sobre el primer elemento, aunque
+	// haya mas en la seleccion)
+	console.log($a.attr('href'));
+
+	// Metodo Setter (opera sobre todos los elementos, si hay
+	// mas de uno en la seleccion)
+	$a.attr('href', 'http://www.google.com')
+	console.log($a.attr('href'));
+
+	var $h1 = $('h1');
+
+	$h1.addClass('danger');
+
+	setTimeout(function() {
+		$h1.toggleClass('danger');
+	}, 1500);
+
+	// No es lo mismo un jQuery Object que un elemento del DOM
+	var $h1 = $('h1');
+	var $h1b = $('h1');
+	console.log('$h1 === $h1b: ', $h1 === $h1b);
+	console.log('$h1[0] === $h1b[0]: ', $h1[0] === $h1b[0]);
+
+	var $h1 = $('h1');
+	$h1.css({
+		'font-size': '70px'
+	});
 })
 
 // Si hay prototype y jQuery se pueden pisar entre ellos
