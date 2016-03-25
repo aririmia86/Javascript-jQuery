8b05dd268cba0304ec81941c7151401a9ce283b0
Alejandro Irimia <rimax_86@hotmail.com>
2016-03-25 19:36:11 +0100

    Callbacks y efecto spinner

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 $(function () {
-	/**
-	* Submit search form
-	*/
-	$('#app-body')
-		.find('form')
-		.submit(function (ev) {
-			ev.preventDefault();
-			// This es el elemento form del DOM. Al rodearlo de
-			// $ lo convertimos en un jQuery Object
-			var $busqueda = $(this)
-				.find('input[type="text"]')
-				.val();
-			alert("Se ha buscado " + $busqueda);
-		})
-
+	var $container = $('#app-body').find('.tv-shows');
 	var template = '<article class="tv-show">' + 
 					'<div class="left img-container">' + 
 						'<img src=":img:" alt=":img alt:">' +
@@ -23,22 +9,59 @@ $(function () {
 						'<p>:summary:</p>' +
 					'</div>' +
 				'</article>';
+	function renderShows (shows) {
+		shows.forEach(function (show){
+			var article = template
+				.replace(':name:', show.name)
+				.replace(':img:', show.image.medium)
+				.replace(':summary:', show.summary)
+				.replace(':img alt:', show.name + " Logo");
+			// Conversion a jQuery Object
+			var $article = $(article);
+			// Lo ocultamos por defecto
+			$article.hide();
+			// Seleccion de jQuery
+			$container.append($article);
+			$article.show(1500);
+		})
+	}
+	/**
+	* Submit search form
+	*/
+	$('#app-body')
+		.find('form')
+		.submit(function (ev) {
+			ev.preventDefault();
+			// This es el elemento form del DOM. Al rodearlo de
+			// $ lo convertimos en un jQuery Object
+			var busqueda = $(this)
+				.find('input[type="text"]')
+				.val();
+			$container.find('.tv-show').remove();
+			var $loader = $('<div class="loader"></div>');
+			$loader.appendTo($container);
+			$.ajax({
+				url: 'http://api.tvmaze.com/search/shows',
+				data: { q: busqueda},
+				success: function (res, textStatus, xhr){
+					console.log(res);
+					$loader.remove();
+					var shows = res.map(function (element) {
+						return element.show;
+					})
+					console.log(shows);
+					renderShows(shows);
+				}
+			})
+		})
 	// Request AJAX a API de TVMaze
 	// Metodo por defecto GET
 	$.ajax('http://api.tvmaze.com/shows', {
 		'success': function (shows, textStatus, xhr) {
-			var $container = $('#app-body').find('.tv-shows');
+			$container.find('.loader').remove();
 			// forEach recibe por parametro una funcion que se
 			// llama por cada elemento
-			shows.forEach(function (show){
-				var article = template
-					.replace(':name:', show.name)
-					.replace(':img:', show.image.medium)
-					.replace(':summary:', show.summary)
-					.replace(':img alt:', show.name + " Logo");
-				// Seleccion de jQuery
-				$container.append($(article));
-			})
+			renderShows(shows);
 		}
 	})
 })
